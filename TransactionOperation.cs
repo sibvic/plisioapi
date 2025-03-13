657e4dd0217a70f79bfe45399744f07ea192b8db
Victor Tereschenko <sibvic@gmail.com>
2025-03-13 16:08:20 +0600

    feat: Forgotten file

---
+++ TransactionOperation.cs
@@ -0,0 +1,15 @@
+﻿namespace Sibvic.plisioapi;
+
+public class TransactionOperation
+{
+    public string? Txn_Id { get; set; }
+    public string? Invoice_Url { get; set; }
+    public decimal? Invoice_Total_Sum { get; set; }
+    public int User_Id { get; set; }
+    public string? Shop_Id { get; set; }
+    public string? Type { get; set; }
+    public string? Status { get; set; }
+    public string[] Tx_Url { get; set; }
+    public string? Id { get; set; }
+}
+
